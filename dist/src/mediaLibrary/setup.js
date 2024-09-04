dbb29ee5b0a51c3ad4c62ac871b7f1f7b50634b2
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-09-04 16:34:22 +0530

    before drive

--- dist/src/mediaLibrary/setup.js
+++ dist/src/mediaLibrary/setup.js
@@ -1,64 +1,61 @@
 import "../../node_modules/xyzWidget/dist/bundle.js";
 
-document.getElementById("open-btn").addEventListener("click", () => {
-	window.pixelbin.createMediaLibrary(
-		{
-			dev: true,
-			cloud_name: "polished-hat-8f9bd4",
-			pixelbinURL: "https://console.pixelbinz0.de",
-			remove_header: false,
-			max_files: "1",
-			insert_caption: "Insert",
-			inline_container: "#widget_container",
-			default_transformations: [[]],
-			button_class: "myBtn",
-			button_caption: "Select Image or Video",
-		},
-		{
-			insertHandler: function (data) {
-				if (data && data.assets) {
-					data.assets.forEach((asset) => {
-						const url = asset.url;
-						console.log("Inserted asset:", JSON.stringify(asset, null, 2));
+// document.getElementById("open-btn").addEventListener("click", () => {
+window.pixelbin.createMediaLibrary(
+	{
+		dev: true,
+		cloud_name: "polished-hat-8f9bd4",
+		pixelbinURL: "https://console.pixelbinz0.de",
+		remove_header: false,
+		max_files: "1",
+		insert_caption: "Insert",
+		inline_container: "#widget_container",
+		default_transformations: [[]],
+		button_class: "myBtn",
+		button_caption: "Select Image or Video",
+	},
+	{
+		insertHandler: function (data) {
+			if (data && data.assets) {
+				data.assets.forEach((asset) => {
+					const url = asset.url;
+					console.log("Inserted asset:", JSON.stringify(asset, null, 2));
 
-						chrome.runtime.sendMessage(
-							{ type: "getActiveTabId" },
-							(response) => {
-								if (response && response.activeTabId) {
-									chrome.scripting
-										.executeScript({
-											target: { tabId: response.activeTabId },
-											func: (url) => {
-												const activeElement = document.activeElement;
-												if (
-													activeElement &&
-													activeElement.tagName === "INPUT" &&
-													activeElement.type === "text"
-												) {
-													activeElement.value = url;
-												} else if (
-													activeElement &&
-													activeElement.isContentEditable
-												) {
-													activeElement.innerHTML = url;
-												} else {
-													alert("Active element is not editable");
-												}
-											},
-											args: [url],
-										})
-										.catch((error) => {
-											console.error("Script injection failed: ", error);
-										});
-								} else {
-									console.error("Could not retrieve active tab ID");
-								}
-							}
-						);
+					chrome.runtime.sendMessage({ type: "getActiveTabId" }, (response) => {
+						if (response && response.activeTabId) {
+							chrome.scripting
+								.executeScript({
+									target: { tabId: response.activeTabId },
+									func: (url) => {
+										const activeElement = document.activeElement;
+										if (
+											activeElement &&
+											activeElement.tagName === "INPUT" &&
+											activeElement.type === "text"
+										) {
+											activeElement.value = url;
+										} else if (
+											activeElement &&
+											activeElement.isContentEditable
+										) {
+											activeElement.innerHTML = url;
+										} else {
+											alert("Active element is not editable");
+										}
+									},
+									args: [url],
+								})
+								.catch((error) => {
+									console.error("Script injection failed: ", error);
+								});
+						} else {
+							console.error("Could not retrieve active tab ID");
+						}
 					});
-				}
-			},
+				});
+			}
 		},
-		document.getElementById("open-btn")
-	);
-});
+	}
+	// document.getElementById("open-btn")
+);
+// });
